dc1a3d6b996b9e378f88c6a76f0a879831a5ffd7
Daniel <herrnichols@gmail.com>
2019-10-14 09:24:48 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    	This is a test of Vim.  
+	  </header>
+    </div>
+  );
+}
+
+export default App;
