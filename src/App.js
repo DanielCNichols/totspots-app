7ecab679310c8fc3e1c3f42b4ecf35098d8e49a4
Daniel <herrnichols@gmail.com>
2019-10-25 09:37:18 -0400

    finished styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ResultsPage from './ResultsPage/ResultsPage';
 import Nav from './Nav/Nav';
 import Main from './Main/Main'
 import ProfileView from './Profileview/Profileview';
-import AddVenue from './AddVenue/AddVenue'
+import AddVenue from './AddVenue/Addvenue'
 import ReviewForm from './ReviewForm/ReviewForm'
 import VenuesPage from './VenuesPage/VenuesPage'
 import FavoritesList from './FavoritesList/Favoriteslist'
--- src/addvenue/addvenue.js
+++ src/addvenue/addvenue.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ApiService from '../services/api-service';
 import { withRouter } from 'react-router-dom';
-import './AddVenue.css'
+import './Addvenue.css'
 
 class AddVenue extends React.Component {
   state = {
