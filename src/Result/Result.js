30b9177804a44eee60c0e884057dc73985dc7d01
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2020-07-15 15:27:26 -0400

    refactored Result.js into functional component.

--- src/Result/Result.js
+++ src/Result/Result.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import VenueContext from '../VenuesContext';
 import { withRouter } from 'react-router-dom';
 import s from './Result.module.css';
 import Rating from '../Rating/Rating';
 import config from '../config';
 
-class Result extends React.Component {
-  static contextType = VenueContext;
+//TODO: refactor this into a functional component.
+//TODO: Get styles out of the component
 
-  handleExpanded(venueId) {
-    this.context.setSelectedVenue(venueId);
-    this.props.history.push(`/venue/${venueId}`);
-  }
+const Result = props => {
+  let context = useContext(VenueContext);
+  const [expanded, setExpanded] = useState(false);
 
-  renderTypes() {
-    let { types } = this.props.venue;
-
+  function renderTypes() {
+    let { types } = props.venue;
     let formatted = types.map(t =>
       t !== 'establishment' && t !== 'point_of_interest'
         ? t.split('_').join(' ')
@@ -36,34 +34,34 @@ class Result extends React.Component {
     });
   }
 
-  render() {
-    let { venue } = this.props;
-    return (
-      <li
-        className={s.result}
-        key={venue.id}
-        onClick={() => this.handleExpanded(venue.id)}
-      >
-        {venue.photos ? (
-          <img
-            src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${venue.photos[0].photo_reference}&key=${config.GKEY}
+  let { venue } = props;
+
+  return (
+    <li
+      className={s.result}
+      key={venue.id}
+      // onClick={() => this.handleExpanded(venue.id)}
+    >
+      {venue.photos ? (
+        <img
+          src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${venue.photos[0].photo_reference}&key=${config.GKEY}
 `}
-            alt="google"
-          />
-        ) : null}
-        <h4>{venue.name}</h4>
-        <div className={s.ratings}>
-          <Rating value={venue.rating} symbol="&#x2605;" />
-          <Rating
-            className="price_span"
-            value={venue.price_level}
-            symbol="&#36;"
-          />
-        </div>
-        <span className={s.address}>{venue.vicinity}</span>
-        <span className={s.types}>{this.renderTypes()}</span>
-      </li>
-    );
-  }
-}
+          alt="google"
+        />
+      ) : null}
+      <h4>{venue.name}</h4>
+      <div className={s.ratings}>
+        <Rating value={venue.rating} symbol="&#x2605;" />
+        <Rating
+          className="price_span"
+          value={venue.price_level}
+          symbol="&#36;"
+        />
+      </div>
+      <span className={s.address}>{venue.vicinity}</span>
+      <span className={s.types}>{renderTypes()}</span>
+    </li>
+  );
+};
+
 export default withRouter(Result);
