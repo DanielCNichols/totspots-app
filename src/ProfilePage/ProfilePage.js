f5c3a646c136d1c8a2de4fe069c4b0eab6d1f4d9
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2020-07-29 15:06:22 -0400

    Added mobile layout for profile page

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -3,13 +3,10 @@ import ApiService from '../services/api-service';
 import s from './ProfilePage.module.css';
 import Favorite from '../Favorite/Favorite';
 import UserReview from '../UserReview/UserReview';
+import { MdEdit } from 'react-icons/md';
 
 //TODO: Update reviews so that only one review can be made per user (then, conditionally render edit button on venue page instead of "write review"
 //Todo: Refactor the page:
-//! convert to functional component
-//! Display user info
-//First Name, Last Name, Email, Option to edit
-//Total Reviews, Total Karma would be cool.
 //! Display basicVenueInfo
 //! Display all review info for venue
 //! Link review/venue to detailed venue page
@@ -22,12 +19,15 @@ const ProfilePage = props => {
   let [showReviews, setShowReviews] = useState(false);
   let [reviews, setReviews] = useState([]);
   let [favorites, setFavorites] = useState([]);
+  let [user, setUser] = useState({});
 
   useEffect(() => {
     ApiService.getProfile()
       .then(res => {
+        console.log(res);
         setReviews(res.reviews);
         setFavorites(res.favorites);
+        setUser(res.user);
         setLoading(false);
       })
       .catch(err => {
@@ -49,99 +49,62 @@ const ProfilePage = props => {
   }
 
   return (
-    <section style={{ marginTop: '100px' }} className={s.profilePage}>
+    <section className={s.profilePage}>
+      <header>
+        <h2>Profile</h2>
+      </header>
       <div className={s.profileCard}>
-        <h3>Daniel Nichols</h3>
-        <p>herrnichols@gmail.com</p>
-        <button>Edit Profile</button>
+        <div className={s.profileHeader}>
+          <h3>
+            {user.first_name} {user.last_name}
+          </h3>
+          <button>
+            <MdEdit />
+            <span>Edit Profile</span>
+          </button>
+          <p>{user.email}</p>
+        </div>
         <div className={s.userStats}>
-          <div>
-            <p>Total reviews: 32</p>
-          </div>
-          <div>
-            <p>Total Karma: 123</p>
-          </div>
+          {user.count > 0 && <p>Total Reviews: {user.count}</p>}
         </div>
       </div>
 
-      <div className={s.displayControls}>
-        <button onClick={() => setShowReviews(!showReviews)}>
-          Show Reviews
-        </button>
+      <div className={s.activityList}>
+        <nav className={s.tabNav}>
+          <ul>
+            <li onClick={() => setShowReviews(false)}>Favorites</li>
+            <li onClick={() => setShowReviews(true)}>Reviews</li>
+          </ul>
+        </nav>
+
+        {!showReviews && (
+          <ul className={s.favorites}>
+            <h3>Saved Spots</h3>
+
+            {favorites.map(favorite => {
+              return (
+                <Favorite
+                  key={favorite.venueid}
+                  deleteFavorite={handleRemoveFavorite}
+                  favorite={favorite}
+                />
+              );
+            })}
+          </ul>
+        )}
+
+        {showReviews && (
+          <ul className={s.reviews}>
+            <h3>Review History</h3>
+            {reviews.map(review => {
+              console.log(review);
+              return <UserReview key={review.id} review={review} />;
+            })}
+          </ul>
+        )}
       </div>
-
-      {!showReviews && (
-        <ul className={s.favorites}>
-          {favorites.map(favorite => {
-            return (
-              <Favorite
-                key={favorite.venueid}
-                deleteFavorite={handleRemoveFavorite}
-                favorite={favorite}
-              />
-            );
-          })}
-        </ul>
-      )}
-
-      {showReviews && (
-        <ul className={s.reviews}>
-          {reviews.map(review => {
-            console.log(review);
-            return <UserReview key={review.id} review={review} />;
-          })}
-        </ul>
-      )}
     </section>
   );
 };
-// export default class ProfileView extends React.Component {
-// static contextType = venuesContext;
-// componentDidMount() {
-//   this.context.clearError();
-//   ApiService.getProfile()
-//     .then(profile => {
-//       this.context.setProfile(profile);
-//     })
-//     .catch(this.context.setError);
-// }
-// componentWillUnmount() {
-//   this.context.clearError();
-// }
-// renderError() {
-//   let error = this.context.error;
-//   if (this.context.error) {
-//     return (
-//       <div className="error">
-//         <p>Sorry something has gone wrong. {error.error}</p>
-//       </div>
-//     );
-//   }
-// }
-// render() {
-//   const { profile } = this.context;
-//   return (
-//     <section className="profileview">
-//       <header>
-//         <h3>Your Profile</h3>
-//       </header>
-//       <div role="alert">{this.renderError()}</div>
-//       <div className="profile_info">
-//         <p>
-//           Name: {profile.first_name} {profile.last_name}
-//         </p>
-//         <p>Email: {profile.email}</p>
-//         <p>UserName: {profile.username}</p>
-//       </div>
-//       <div>
-//         <FavoritesList></FavoritesList>
-//       </div>
-//       <div>
-//         <ReviewsList></ReviewsList>
-//       </div>
-//     </section>
-//   );
-// }
-// }
 
 export default ProfilePage;
