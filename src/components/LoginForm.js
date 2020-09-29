f10a97cd932981295b5e853aae95ce9a948ef5bb
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2020-09-29 12:32:22 -0400

    updated login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,24 +7,99 @@ import {
   Fieldset,
   Button,
 } from '../components/Form';
+import AuthService from '../services/AuthService';
 import {withRouter} from 'react-router-dom';
 // import s from '../sass/components/LoginForm.module.scss';
 
 //Todo: LoginForm redirects to homepage if the login is completed on the registration page.
 
 export function LoginForm(props) {
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    username: null,
+    passwoard: null,
+    server: null,
+  });
+
+  const handleInputChange = event => {
+    event.persist();
+    setInputs(inputs => ({
+      ...inputs,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const validateInputs = () => {
+    let {username, password} = inputs;
+    console.log(username, password);
+    let errors = {};
+
+    if (!username) {
+      errors.username = 'Username is required';
+    }
+
+    if (!password) {
+      errors.password = 'Password is required';
+    }
+
+    return errors;
+  };
+
+  const clearErrors = () => {
+    setErrors({
+      username: null,
+      password: null,
+      server: null,
+    });
+  };
+
+  const handleLoginSubmit = async ev => {
+    ev.preventDefault();
+    clearErrors();
+
+    let errors = validateInputs();
+
+    if (errors.username || errors.password) {
+      return setErrors(errors);
+    }
+    console.log(inputs);
+    let res = await AuthService.postLogin(inputs);
+
+    console.log(res);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleLoginSubmit}>
       <Fieldset>
         <Legend>Log in</Legend>
         <FormElement>
           <Label htmlFor='username'>Username</Label>
-          <Input name='username' id='username' type='text' />
+          {errors.username && <p>{errors.username}</p>}
+          <Input
+            name='username'
+            id='username'
+            type='text'
+            value={inputs.username}
+            onChange={handleInputChange}
+          />
         </FormElement>
         <FormElement>
           <Label htmlFor='password'>Password</Label>
-          <Input name='password' id='password' type='password' />
+          {errors.password && <p>{errors.password}</p>}
+          <Input
+            name='password'
+            id='password'
+            type='password'
+            value={inputs.password}
+            onChange={handleInputChange}
+          />
         </FormElement>
+
+        {errors.server && <p>{errors.server}</p>}
       </Fieldset>
 
       <Button>Submit</Button>
--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export default {
-  // API_ENDPOINT: "https://radiant-island-73758.herokuapp.com/api",
-  API_ENDPOINT: 'http://localhost:8000/api',
-  API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+  API_KEY: process.env.REACT_APP_API_KEY || 'totspots-key',
   TOKEN_KEY: 'bearer',
-  GKEY: 'AIzaSyDVvbBDpz_vwsfJcTkavRogyXywBHCKqyA',
+  GKEY: process.env.GKEY || 'key',
 };
